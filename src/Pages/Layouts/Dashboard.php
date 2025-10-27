aa0d08f4c88bd5679666d0a3776e59d8cd5526a5
Kurniawan Pratama <kurama.career@gmail.com>
2025-10-27 23:02:08 +0700

    Perbaikan UI halaman USERS

--- src/Pages/Layouts/Dashboard.php
+++ src/Pages/Layouts/Dashboard.php
@@ -6,6 +6,8 @@ class Dashboard
 {
     public static function get($content, $title = "POS425"): string
     {
+        $isBold = fn($path) => str_contains($_SERVER['PATH_INFO'], $path) ? "font-bold" : "";
+
         ob_start(); ?>
         <!DOCTYPE html>
         <html lang="en">
@@ -22,11 +24,13 @@ class Dashboard
                 <nav class="p-2">
                     <ul class="text-white flex flex-row gap-7 h-12 items-center">
                         <li class="text-xl font-bold"><a href="/dashboard/users">POS425</a></li>
-                        <li class="ml-auto"><a href="/dashboard/users">USERS</a></li>
-                        <li><a href="/dashboard/user-roles">ROLES</a></li>
-                        <li><a href="/dashboard/products">PRODUCTS</a></li>
-                        <li><a href="/dashboard/product-categories">CATEGORIES</a></li>
-                        <li class="mr-auto"><a href="/dashboard/transactions">TRANSACTIONS</a></li>
+                        <li class="ml-auto <?= $isBold('/users') ?>"><a href="/dashboard/users">USERS</a></li>
+                        <li class="<?= $isBold('/user-roles') ?>"><a href="/dashboard/user-roles">ROLES</a></li>
+                        <li class="<?= $isBold('/products') ?>"><a href="/dashboard/products">PRODUCTS</a></li>
+                        <li class="<?= $isBold('/product-categories') ?>"><a href="/dashboard/product-categories">CATEGORIES</a>
+                        </li>
+                        <li class="mr-auto <?= $isBold('/transactions') ?>"><a href="/dashboard/transactions">TRANSACTIONS</a>
+                        </li>
                         <li><a href="/dashboard/transactions">LOGOUT</a></li>
                     </ul>
                 </nav>
@@ -35,6 +39,6 @@ class Dashboard
         </body>
 
         </html>
-<?php return ob_get_clean();
+        <?php return ob_get_clean();
     }
 } ?>
--- src/Pages/Views/Users.php
+++ src/Pages/Views/Users.php
@@ -5,6 +5,7 @@ namespace App\Pages\Views;
 use App\Config\Database;
 use App\Pages\Layouts\Dashboard;
 use App\Utils\Message;
+use DateTime;
 ?>
 
 <?php class Users
@@ -22,14 +23,14 @@ use App\Utils\Message;
     private function getUserRolesCols()
     {
         return "
-        <tr class='[&_th]:border [&_th]:px-1.5 [&_th]:py-3 bg-emerald-200'>
-            <th>row</th>
-            <th>name</th>
-            <th>role name</th>
-            <th>email</th>
-            <th>created at</th>
-            <th>updated at</th>
-            <th>actions</th>
+        <tr>
+            <th class='text-left'>row</th>
+            <th class='text-left'>name</th>
+            <th class='text-center'>role name</th>
+            <th class='text-center'>email</th>
+            <th class='text-left'>created at</th>
+            <th class='text-left'>updated at</th>
+            <th class='text-center'>actions</th>
         </tr>
         ";
     }
@@ -48,15 +49,60 @@ use App\Utils\Message;
         foreach ($userRolesDatas as $key => $row) {
             $key += 1;
 
+            $no = str_pad($key, 2, "0", STR_PAD_LEFT);
+
+            $bg_role_name = "";
+            switch (strtolower($row['role_name'])) {
+                case 'superadmin':
+                    $bg_role_name = 'bg-emerald-100 text-emerald-600';
+                    break;
+                case 'admin':
+                    $bg_role_name = 'bg-blue-100 text-blue-600';
+                    break;
+                case 'operator':
+                    $bg_role_name = 'bg-yellow-100 text-yellow-600';
+                    break;
+
+                default:
+                    $bg_role_name = 'bg-purple-100 text-purple-600';
+                    break;
+            }
+
+            $created_at = new DateTime($row['created_at']);
+            $created_at_date = date_format($created_at, 'd/m/Y');
+            $created_at_time = date_format($created_at, 'h:i:s');
+
+            $updated_at = new DateTime($row['updated_at']);
+            $updated_at_date = date_format($updated_at, 'd/m/Y');
+            $updated_at_time = date_format($updated_at, 'h:i:s');
+
             $rows[] = "
             <tr>
-                <td style='border: 1px solid black'>$key</td>
-                <td style='border: 1px solid black'>{$row['name']}</td>
-                <td style='border: 1px solid black'>{$row['role_name']}</td>
-                <td style='border: 1px solid black'>{$row['email']}</td>
-                <td style='border: 1px solid black'>{$row['created_at']}</td>
-                <td style='border: 1px solid black'>{$row['updated_at']}</td>
-                <td style='border: 1px solid black'>{$this->editAndDelete($row['id'])}</td>
+                <td>$no</td>
+                <td class='uppercase'>{$row['name']}</td>
+                <td>
+                    <div class='flex items-center justify-center text-sm'>
+                        <p class='w-fit px-4 py-2 font-mono rounded-full $bg_role_name'>{$row['role_name']}</p>
+                    </div>
+                </td>
+                <td>
+                    <div class='flex items-center justify-center font-mono'>
+                        <p class='text-sm text-black px-4 py-2 bg-slate-200 rounded-full w-fit text-center'>{$row['email']}</p>
+                    </div>
+                </td>
+                <td class='text-sm font-mono'>
+                    <div class='flex flex-col'>
+                        <span>$created_at_date</span>
+                        <span>$created_at_time</span>
+                    </div>
+                </td>
+                <td class='text-sm font-mono'>
+                    <div class='flex flex-col'>
+                        <span>$updated_at_date</span>
+                        <span>$updated_at_time</span>
+                    </div>
+                </td>
+                <td><div id='edit-and-delete-{$row['id']}'>{$this->editAndDelete($row['id'])}</div></td>
             </tr>
             ";
         }
@@ -70,16 +116,16 @@ use App\Utils\Message;
         ob_start(); ?>
 
         <!-- DELETE -->
-        <form method="POST" action="/dashboard/users/q/<?= $id ?>">
+        <form id="button-delete-<?= $id ?>" method="POST" action="/dashboard/users/q/<?= $id ?>">
             <input type="hidden" name="_SECURITY_" value="1234567890">
             <input type="hidden" name="_METHOD_" value="DELETE">
             <button type="submit">DELETE</button>
         </form>
 
         <!-- EDIT -->
-        <a href="/dashboard/users/q/<?= $id ?>">EDIT</a>
+        <a id="button-edit-<?= $id ?>" href="/dashboard/users/q/<?= $id ?>">EDIT</a>
 
-    <?php
+        <?php
         return ob_get_clean();
     }
 
@@ -105,48 +151,78 @@ use App\Utils\Message;
 
         $message = Message::get();
         ob_start() ?>
-        <div class="">
+        <div>
             <section>
                 <?php if ($message): ?>
-                    <span id="message" class="<?= $message['success'] ? "bg-emerald-200" : "bg-red-200" ?> fixed top-14 left-1/2 -transalate-x-1/2">
+                    <span id="message"
+                        class="<?= $message['success'] ? "bg-emerald-200" : "bg-red-200" ?> fixed top-14 left-1/2 -transalate-x-1/2">
                         <?= $message['message'] ?>
                     </span>
                 <?php endif ?>
             </section>
-            <div class="flex flex-row">
-                <section class="order-1 basis-1/3">
-                    <form action="<?= $formActions ?>" method="POST">
+            <div>
+                <section id="wrapper-add-and-update"
+                    class="hidden items-center justify-center fixed top-0 left-0 w-full h-full bg-slate-100/20 backdrop-blur-md">
+                    <form id="add-and-update" action="<?= $formActions ?>" method="POST">
+                        <h2 class="text-black text-center text-2xl font-bold font-serif">
+                            <?= !$this->id ? "TAMBAH" : "EDIT" ?> USER
+                        </h2>
                         <input type="hidden" name="_SECURITY_" value="1234567890">
                         <?php if ($this->id): ?>
                             <input type="hidden" name="_METHOD_" value="PUT">
                         <?php endif ?>
 
-                        <input type="text" name="name" value="<?= $result['name'] ?? '' ?>" placeholder="Fullname" autocorrect="off"
-                            autocomplete="off">
+                        <label for="name">
+                            <span>Full Name <small>*</small></span>
+                            <input type="text" name="name" value="<?= $result['name'] ?? '' ?>" placeholder="Fullname"
+                                autocorrect="off" autocomplete="off">
+                        </label>
 
-                        <select name="role_id">
-                            <option value="" <?= $this->id ? "" : "selected" ?>>-- Pilih Role --</option>
-                            <?php
-                            $getRoleIDfromResult = $result['role_id'] ?? 0;
-                            ?>
-                            <?php foreach ($stmtRolesResult as $rolesResult): ?>
-                                <option value="<?= $rolesResult['id'] ?? '' ?>" <?= $rolesResult['id'] == $getRoleIDfromResult ? "selected" : "" ?>><?= $rolesResult['name'] ?></option>
-                            <?php endforeach ?>
-                        </select>
+                        <label for="role_id">
+                            <span>Role Name <small>*</small></span>
+                            <select name="role_id">
+                                <option value="" <?= $this->id ? "" : "selected" ?>>-- Pilih Role --</option>
+                                <?php
+                                $getRoleIDfromResult = $result['role_id'] ?? 0;
+                                ?>
+                                <?php foreach ($stmtRolesResult as $rolesResult): ?>
+                                    <option value="<?= $rolesResult['id'] ?? '' ?>" <?= $rolesResult['id'] == $getRoleIDfromResult ? "selected" : "" ?>><?= $rolesResult['name'] ?></option>
+                                <?php endforeach ?>
+                            </select>
+                        </label>
 
-                        <input type="text" name="email" value="<?= $result['email'] ?? '' ?>" placeholder="your@email.com"
-                            autocorrect="off" autocomplete="off">
+                        <label for="email">
+                            <span>Email <small>*</small></span>
+                            <input type="text" name="email" value="<?= $result['email'] ?? '' ?>" placeholder="your@email.com"
+                                autocorrect="off" autocomplete="off">
+                        </label>
 
-                        <input type="text" name="password" value="" placeholder="********" autocorrect="off" autocomplete="off">
+                        <label for="password">
+                            <span>Password <small>*</small></span>
+                            <input type="text" name="password" value="" placeholder="********" autocorrect="off"
+                                autocomplete="off">
+                        </label>
 
-                        <button type="submit"><?= $btnUpdateUsers ?></button>
+                        <div class="wrapper-button">
+                            <a class="bg-red-100 text-red-600" href="/dashboard/users">CANCEL</a>
+                            <button class="<?= !$this->id ? "bg-emerald-100 text-emerald-600" : "bg-blue-100 text-blue-600" ?>"
+                                type="submit"><?= $btnUpdateUsers ?></button>
+                        </div>
                     </form>
                 </section>
-                <section class="order-2 basis-2/3 p-3">
-                    <table class="w-full">
-                        <?= $this->getUserRolesCols() ?>
-                        <?= $this->getUserRolesRows() ?>
-                    </table>
+                <section>
+                    <div class="max-w-dvw overflow-x-auto">
+                        <div class="flex flex-row justify-between p-2">
+                            <label for="search" id="search" class="bg-slate-100 p-3 rounded">
+                                <input type="text" name="search" placeholder="search user">
+                            </label>
+                            <button onclick="toggleFormAddAndUpdate()" type="button" id="button-new">New User</button>
+                        </div>
+                        <table class="w-full">
+                            <?= $this->getUserRolesCols() ?>
+                            <?= $this->getUserRolesRows() ?>
+                        </table>
+                    </div>
                 </section>
             </div>
         </div>
@@ -157,8 +233,20 @@ use App\Utils\Message;
                     messageElement.remove()
                 }, 1500);
             }
+
+            const elementWrapperAddAndUpdate = document.getElementById('wrapper-add-and-update');
+            const path = window.location.pathname;
+
+            if (path.match("/users/q")) {
+                elementWrapperAddAndUpdate.classList.replace("hidden", 'flex');
+            }
+
+            const toggleFormAddAndUpdate = () => {
+                elementWrapperAddAndUpdate.classList.toggle("hidden");
+                elementWrapperAddAndUpdate.classList.toggle("flex");
+            }
         </script>
-<?= Dashboard::get(ob_get_clean(), "Users");
+        <?= Dashboard::get(ob_get_clean(), "Users");
     }
 
     public function __destruct()
